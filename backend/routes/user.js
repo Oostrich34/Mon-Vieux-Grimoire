001fd972c14abdc4cb1284ea966731adca0e8d8f
Alexandre Peyrolles <apeyrolles34@gmail.com>
2025-12-15 16:47:19 +0100

    feat:  Ajout de commentaires pour une meilleure compréhension du code et début de l'ajout de fonctionnalité de notation des livres

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
+// backend/routes/user.js
 const express = require('express');
 
+// Router Express pour les routes des utilisateurs
 const router = express.Router();
 
+// Importer le contrôleur des utilisateurs
 const userCtrl = require('../controllers/user');
 
+// Routes pour les utilisateurs
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
